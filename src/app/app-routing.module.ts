cb9e22b8335c143ac5b399c77ee56e96b51b9df2
Caroline <caroline@curieos.com>
2019-10-28 11:57:59 -0500

    Add a module model and a list view for modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PlantCreateComponent } from './plant/plant-create/plant-create.component';
 import { PlantListComponent } from './plant/plant-list/plant-list.component';
+import { ModuleLinkComponent } from './module/module-link/module-link.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'plants', component: PlantListComponent },
   { path: 'plants/create', component: PlantCreateComponent },
+  { path: 'modules', component: ModuleListComponent },
 ];
 
 @NgModule({
--- src/app/plant/plant.model.ts
+++ src/app/plant/plant.model.ts
@@ -1,4 +1,7 @@
+import { Module } from '../module/module.model';
+
 export interface Plant {
   _id: string;
   name: string;
+  module: Module;
 }
