895ca5f929155e43ea0120853d1b8cd91fb9b3ab
Caroline <caroline@curieos.com>
2019-10-28 11:55:31 -0500

    Edit the header to include useful functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlantCreateComponent } from './plant/plant-create/plant-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'plants/create', component: PlantCreateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlantCreateComponent } from './plant/plant-create/plant-create.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlantCreateComponent,
+    HeaderComponent,
   ],
   imports: [
     NgbModule,
