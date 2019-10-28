babdd9ce22cdacc332590566e21283e3590818d6
Caroline <caroline@curieos.com>
2019-10-28 11:56:07 -0500

    Add a dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { PlantCreateComponent } from './plant/plant-create/plant-create.component';
 
 const routes: Routes = [
+  { path: '', component: DashboardComponent },
   { path: 'plants/create', component: PlantCreateComponent },
 ];
 
