b5f6137639bcb8c17109f07f8f00bba3e8a220f7
Caroline <caroline@curieos.com>
2019-11-01 20:31:19 -0500

    Switch over to a SEAN stack with sequelize and sqlite

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const env = require("node-env-file");
 
 env("./.env");
@@ -9,13 +8,6 @@ env("./.env");
 const plantsRoutes = require("./routes/plants")
 
 const app = express();
-
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-	console.log("Connected to Database");
-}).catch((error) => {
-	console.log("Connection to db failed!");
-	console.log(error);
-});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
