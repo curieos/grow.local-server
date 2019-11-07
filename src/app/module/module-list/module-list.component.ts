aff9481758b9d5506ce9914a965f158ae42fe7ff
Caroline <caroline@curieos.com>
2019-11-06 22:14:24 -0600

    Edit the header and list pages to use a button on the list pages to add

--- src/app/module/module-list/module-list.component.ts
+++ src/app/module/module-list/module-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Module } from '../module.model';
 
 @Component({
   selector: 'app-module-list',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./module-list.component.css']
 })
 export class ModuleListComponent implements OnInit {
+  public moduleList: Module[];
 
   constructor() { }
 
