a2ca257f8ed7f2624eb1297399e557644d582709
Caroline <cvrieous@gmail.com>
2020-06-09 15:44:16 -0500

    Fix linting issues

--- src/app/error.interceptor.spec.ts
+++ src/app/error.interceptor.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ErrorInterceptor } from './error.interceptor';
 
 describe('ErrorInterceptor', () => {
   beforeEach(() => TestBed.configureTestingModule({
     providers: [
-      ErrorInterceptor
-      ]
+      ErrorInterceptor,
+    ],
   }));
 
   it('should be created', () => {
