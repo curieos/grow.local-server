a8f027499af55d74fe09e01887b5df5f5b0534d3
Caroline <cvrieous@gmail.com>
2020-06-09 12:52:47 -0500

    Fix e2e test to pass

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to grow-local!');
+    expect(page.getTitleText()).toEqual('Grow.local');
   });
 
   afterEach(async () => {
