88ef041b2e717778febb0df7f81ff14347b06ec2
Caroline <cvrieous@gmail.com>
2020-06-10 12:06:47 -0500

    Edit zone settings

--- src/test.ts
+++ src/test.ts
@@ -3,6 +3,7 @@
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import 'zone.js/dist/zone-testing';
+import './zone-flag';
 
 declare const require: any;
 
