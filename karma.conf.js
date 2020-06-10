4a2ebf7281cad0c0fb073b571aaaa3ede8aabd32
Caroline <cvrieous@gmail.com>
2020-06-09 21:45:05 -0500

    Switch karma reporting output

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/grow-local'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'lcov', 'text-summary'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
