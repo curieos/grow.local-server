3446479e606bde8e3acfb886a069a26dfbfdfbfa
Caroline <cvrieous@gmail.com>
2020-06-09 22:33:17 -0500

    Write more tests for plant model

--- src/app/plant/plant.model.spec.ts
+++ src/app/plant/plant.model.spec.ts
@@ -5,20 +5,20 @@ describe('Plant', () => {
 
   beforeEach(() => {
     plant = new Plant('1', 'Violets', [
-      { value: 24.0, time: '8:30' },
-      { value: 27.0, time: '8:40' },
-      { value: 25.0, time: '8:50' },
-      { value: 25.4, time: '9:00' },
+      { value: 24.0, time: '08:30' },
+      { value: 27.0, time: '08:40' },
+      { value: 25.0, time: '08:50' },
+      { value: 25.4, time: '09:00' },
     ], [
-      { value: 52.2, time: '8:30' },
-      { value: 52.7, time: '8:40' },
-      { value: 51.0, time: '8:50' },
-      { value: 50.0, time: '9:00' },
+      { value: 52.2, time: '08:30' },
+      { value: 52.7, time: '08:40' },
+      { value: 51.0, time: '08:50' },
+      { value: 50.0, time: '09:00' },
     ], [
-      { value: 812, time: '8:30' },
-      { value: 810, time: '8:40' },
-      { value: 800, time: '8:50' },
-      { value: 850, time: '9:00' },
+      { value: 812, time: '08:30' },
+      { value: 810, time: '08:40' },
+      { value: 800, time: '08:50' },
+      { value: 850, time: '09:00' },
     ]);
   });
 
@@ -39,6 +39,13 @@ describe('Plant', () => {
     expect(plant.getCurrentSoilMoisture()).toEqual(850);
   });
 
+  it('should have getCurrent* methods return 0 if no data is available', () => {
+    const emptyPlant = new Plant('1', 'Violets');
+    expect(emptyPlant.getCurrentTemperature()).toEqual(0);
+    expect(emptyPlant.getCurrentHumidity()).toEqual(0);
+    expect(emptyPlant.getCurrentSoilMoisture()).toEqual(0);
+  });
+
   it('should have method getAverageTemperature that returns the average temperature', () => {
     expect(plant.getAverageTemperature()).toEqual(25.35);
   });
@@ -49,5 +56,20 @@ describe('Plant', () => {
 
   it('should have method getAverageSoilMoisture that returns the average soil moisture', () => {
     expect(plant.getAverageSoilMoisture()).toEqual(818);
+  });
+
+  it('should have a static method getPlantHistoryTimestamp that returns a formatted timestamp array', () => {
+    expect(Plant.getPlantHistoryTimestamp(plant.temperatureHistory)).toEqual(['08:30', '08:40', '08:50', '09:00']);
+    expect(Plant.getPlantHistoryTimestamp(plant.humidityHistory)).toEqual(['08:30', '08:40', '08:50', '09:00']);
+    expect(Plant.getPlantHistoryTimestamp(plant.soilMoistureHistory)).toEqual(['08:30', '08:40', '08:50', '09:00']);
+  });
+
+  it('should have a static method getChartData that returns formatted chart data', () => {
+    expect(Plant.getChartData(plant.temperatureHistory, 'Temperature')).toEqual(
+      { data: [24, 27, 25, 25.4], label: 'Temperature', yAxisID: 'temperature' });
+    expect(Plant.getChartData(plant.humidityHistory, 'Humidity')).toEqual(
+      { data: [52.2, 52.7, 51, 50], label: 'Humidity', yAxisID: 'humidity' });
+    expect(Plant.getChartData(plant.soilMoistureHistory, 'Soil Moisture')).toEqual(
+      { data: [812, 810, 800, 850], label: 'Soil Moisture', yAxisID: 'soilMoisture' });
   });
 });
