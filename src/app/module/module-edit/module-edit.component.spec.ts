59817d1d1d361765f71d73cdf3126fceccdc9718
Caroline <cvrieous@gmail.com>
2020-06-11 18:49:45 -0500

    Setup module edit spec

--- src/app/module/module-edit/module-edit.component.spec.ts
+++ src/app/module/module-edit/module-edit.component.spec.ts
@@ -1,11 +1,15 @@
 import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ModulesService } from '../modules.service';
+import { MockModulesService } from '../modules.service.mock';
 import { ModuleEditComponent } from './module-edit.component';
 
 describe('ModuleEditComponent', () => {
   let component: ModuleEditComponent;
   let fixture: ComponentFixture<ModuleEditComponent>;
+  let modulesService: ModulesService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,8 +24,20 @@ describe('ModuleEditComponent', () => {
   }));
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        ModuleEditComponent,
+        { provide: ModulesService, useClass: MockModulesService },
+        { provide: ActivatedRoute,
+          useValue: {
+            snapshot: { params: { id: '1' } },
+          },
+        },
+      ],
+    });
     fixture = TestBed.createComponent(ModuleEditComponent);
     component = fixture.componentInstance;
+    modulesService = TestBed.inject(ModulesService);
     fixture.detectChanges();
   });
 
