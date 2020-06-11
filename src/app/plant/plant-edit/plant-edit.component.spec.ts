8f1d8b2bee6d4d0db1871a68672300edbdfe62cf
Caroline <cvrieous@gmail.com>
2020-06-11 18:49:34 -0500

    Setup plant edit spec

--- src/app/plant/plant-edit/plant-edit.component.spec.ts
+++ src/app/plant/plant-edit/plant-edit.component.spec.ts
@@ -1,11 +1,15 @@
 import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { PlantsService } from '../plants.service';
+import { MockPlantsService } from '../plants.service.mock';
 import { PlantEditComponent } from './plant-edit.component';
 
 describe('PlantEditComponent', () => {
   let component: PlantEditComponent;
   let fixture: ComponentFixture<PlantEditComponent>;
+  let plantsService: PlantsService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,8 +24,20 @@ describe('PlantEditComponent', () => {
   }));
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        PlantEditComponent,
+        { provide: PlantsService, useClass: MockPlantsService },
+        { provide: ActivatedRoute,
+          useValue: {
+            snapshot: { params: { id: '1' } },
+          },
+        },
+      ],
+    });
     fixture = TestBed.createComponent(PlantEditComponent);
     component = fixture.componentInstance;
+    plantsService = TestBed.inject(PlantsService);
     fixture.detectChanges();
   });
 
