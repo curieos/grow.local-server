fcdc94fe8fba43bb0ba2451f278e3513a34137a2
Caroline <cvrieous@gmail.com>
2020-06-11 19:51:06 -0500

    Setup module link component tests

--- src/app/module/module-link/module-link.component.spec.ts
+++ src/app/module/module-link/module-link.component.spec.ts
@@ -3,11 +3,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModulesService } from '../modules.service';
+import { MockModulesService } from '../modules.service.mock';
 import { ModuleLinkComponent } from './module-link.component';
 
 describe('ModuleLinkComponent', () => {
   let component: ModuleLinkComponent;
   let fixture: ComponentFixture<ModuleLinkComponent>;
+  let modulesService: ModulesService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -24,8 +27,15 @@ describe('ModuleLinkComponent', () => {
   }));
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        ModuleLinkComponent,
+        { provide: ModulesService, useClass: MockModulesService },
+      ],
+    });
     fixture = TestBed.createComponent(ModuleLinkComponent);
     component = fixture.componentInstance;
+    modulesService = TestBed.inject(ModulesService);
     fixture.detectChanges();
   });
 
