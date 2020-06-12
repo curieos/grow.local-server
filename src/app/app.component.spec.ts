7dbff50f2df09ee360ff8fbedeb7f3c1da21c672
Caroline <cvrieous@gmail.com>
2020-06-11 20:43:07 -0500

    Migrate to using eslint for everything

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,7 +22,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Grow.local'`, () => {
+  it('should have as title \'Grow.local\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Grow.local');
