05ff59c173f2922573e04533035032afa91ee84a
Caroline <cvrieous@gmail.com>
2020-06-14 09:00:38 -0500

    Fix tests by creating a test host component

--- src/app/list-view/list-view.component.spec.ts
+++ src/app/list-view/list-view.component.spec.ts
@@ -1,25 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Listable } from '../listable';
 import { ListViewComponent } from './list-view.component';
+import { Component, ViewChild } from '@angular/core';
+import { Plant } from '../plant/plant.model';
 
 describe('ListViewComponent', () => {
-  let component: ListViewComponent<Listable>;
-  let fixture: ComponentFixture<ListViewComponent<Listable>>;
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListViewComponent ]
-    })
-    .compileComponents();
+      declarations: [ListViewComponent, TestHostComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ListViewComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create a new list view', () => {
+    expect(component.listViewComponent).toBeTruthy();
   });
+
+  @Component({
+    selector: 'app-host-component',
+    template: '<app-list-view [listItems]="listItems" #list></app-list-view>'
+  })
+  class TestHostComponent {
+    @ViewChild('list')
+    public listViewComponent: ListViewComponent<Plant>;
+    public listItems = [];
+  }
 });
