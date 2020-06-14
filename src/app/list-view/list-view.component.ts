c78daa2abf8a3bf0f88ceaebc79cef2cac12d349
Caroline <cvrieous@gmail.com>
2020-06-14 12:20:05 -0500

    Extract search logic to the list view component

--- src/app/list-view/list-view.component.ts
+++ src/app/list-view/list-view.component.ts
@@ -1,23 +1,42 @@
 import { Component, ContentChild, Input, OnInit, TemplateRef } from '@angular/core';
-import { Listable } from '../listable';
+import { Observable } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
+import { Searchable } from '../searchable';
 
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
   styleUrls: ['./list-view.component.scss']
 })
-export class ListViewComponent<T extends Listable> implements OnInit {
+export class ListViewComponent<T extends Searchable> implements OnInit {
   @Input() public emptyMsg: string;
   @Input() public showPaging = true;
-  @Input() public listItems: Array<T>;
+  public listItems: Array<T>;
+  private items: Array<T>;
   public pageSize = 10;
   public page = 1;
+  public model: any;
 
   @ContentChild(TemplateRef) public itemTemplate: TemplateRef<Element>;
 
+  readonly search = (text$: Observable<string>) => text$.pipe(
+    debounceTime(200),
+    map(term => term === '' ? [] : this.items.filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+  );
+
+  formatter = (x: { name: string }) => x.name;
+
   constructor() { }
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void { }
+
+  @Input()
+  set _listItems(data: Array<T>) {
+    this.listItems = data;
+    this.items = this.listItems;
+  }
+
+  getItemsByName(name: string) {
+    return this.items.filter(v => v.name.toLowerCase().indexOf(name.toLowerCase()) > -1);
   }
 }
