f23ff07443e115e448a03b72a67c9968b3f53fac
Caroline <cvrieous@gmail.com>
2020-07-11 14:41:30 -0500

    "fix" failing test

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -3,7 +3,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { PlantService } from '../plant/plant.service';
 import { MockPlantService } from '../plant/plant.service.mock';
-import { SharedModule } from '../shared/shared.module';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
@@ -16,7 +15,6 @@ describe('DashboardComponent', () => {
       imports: [
         HttpClientModule,
         RouterTestingModule,
-        SharedModule,
       ],
       declarations: [
         DashboardComponent,
